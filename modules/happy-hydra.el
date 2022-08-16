f7d0cd844f60db3fc497c06d67af4ddbe0aae9f0
Justin Garcia <functor@duck.com>
2022-08-16 14:03:27 +0800

    Tab/window hydra switching

--- modules/happy-hydra.el
+++ modules/happy-hydra.el
@@ -1,4 +1,4 @@
-;;; happy-hydra.el --- Hydra definitions for ハッピ Emacs  -*- lexical-binding: t -*-
+;;; happy-hydra.el --- Hydra definitions for ハッピ Emacs  -*- lexical-binding: t; -*-
 ;;
 ;;; Commentary:
 ;;
@@ -31,16 +31,19 @@
   tab-hydra
 
   _j_ : awesome-tab-forward        _k_ : awesome-tab-backward
-  _J_ : awesome-tab-forward-group  _K_ : awesome-tab-backward-group
+  _l_ : awesome-tab-forward-group  _h_ : awesome-tab-backward-group
 
-  _x_ : happy/kill-current-buffer  _q_ : keyboard-quit
+  _x_ : happy/kill-current-buffer
+
+  _q_ : keyboard-quit  _w_ : window-hydra
   "
   ("k" awesome-tab-backward)
-  ("K" awesome-tab-backward-group)
+  ("h" awesome-tab-backward-group)
   ("j" awesome-tab-forward)
-  ("J" awesome-tab-forward-group)
+  ("l" awesome-tab-forward-group)
   ("x" happy/kill-current-buffer)
-  ("q" keyboard-quit :exit t))
+  ("q" keyboard-quit :exit t)
+  ("w" window-hydra/body :exit t))
 
 (defhydra window-hydra (:hint nil)
   "
@@ -50,7 +53,9 @@
   _j_ : windmove-down  _l_ : windmove-right  _v_ : happy/split-window-vertical
   _k_ : windmove-up    _h_ : windmove-left   _b_ : happy/split-window-horizontal
 
-  _x_ : delete-window  _q_ : keyboard-quit
+  _x_ : delete-window
+
+  _q_ : keyboard-quit  _t_ : tab-hydra
   "
   ("j" windmove-down)
   ("k" windmove-up)
@@ -59,10 +64,11 @@
   ("v" happy/split-window-vertical)
   ("b" happy/split-window-horizontal)
   ("x" delete-window)
-  ("q" keyboard-quit :exit t))
+  ("q" keyboard-quit :exit t)
+  ("t" tab-hydra/body :exit t))
 
-(define-key global-map (kbd "C-x t") 'tab-hydra/body)
-(define-key global-map (kbd "C-x w") 'window-hydra/body)
+(define-key global-map (kbd "C-c t") 'tab-hydra/body)
+(define-key global-map (kbd "C-c w") 'window-hydra/body)
 
 (provide 'happy-hydra)
 
