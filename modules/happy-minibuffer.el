57dfdd180d51316992a516c56f17ba72adf4f034
Justin Garcia <functor@duck.com>
2022-08-21 22:41:56 +0800

    Configure corfu and kind-icon

--- modules/happy-minibuffer.el
+++ modules/happy-minibuffer.el
@@ -18,6 +18,23 @@
   (:require marginalia)
   (marginalia-mode))
 
+(setup (:recipe 'corfu)
+  (:require corfu)
+  (:option corfu-auto t)
+  (:with-mode corfu-mode
+    (:hook-into prog-mode)))
+
+(setup (:recipe 'corfu-doc)
+  (:require corfu-doc)
+  (:with-mode corfu-doc-mode
+    (:hook-into corfu-mode)))
+
+(setup (:recipe 'kind-icon)
+  (:require kind-icon)
+  (:option kind-icon-default-face 'corfu-default
+           kind-icon-blend-background nil)
+  (add-to-list 'corfu-margin-formatters #'kind-icon-margin-formatter))
+
 (provide 'happy-minibuffer)
 
 ;;; happy-minibuffer.el ends here
