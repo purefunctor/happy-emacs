bef86443ab44046b7d8728d62797fd28945e1c01
Justin Garcia <functor@duck.com>
2022-08-28 17:14:17 +0800

    Basic completion style for corfu

--- modules/happy-lsp.el
+++ modules/happy-lsp.el
@@ -10,11 +10,18 @@
   (:with-mode yas-minor-mode
     (:hook-into prog-mode)))
 
+(defun happy/lsp-setup-completion ()
+  "Setup `lsp-completion-mode'."
+  (setf (alist-get 'styles (alist-get 'lsp-capf completion-category-defaults)) '(substring)))
+
 (setup (:recipe 'lsp-mode)
   (:option lsp-enable-symbol-highlighting nil
            lsp-headerline-breadcrumb-enable nil
            lsp-modeline-code-actions-enable nil
-           lsp-modeline-diagnositcs-enable nil))
+           lsp-modeline-diagnositcs-enable nil
+           lsp-completion-provider :none)
+  (:with-mode lsp-completion-mode
+    (:hook happy/lsp-setup-completion)))
 
 (provide 'happy-lsp)
 
