a2b33fc1ffc597d0924aee4ab616c918c6d7c790
Justin Garcia <functor@duck.com>
2022-09-17 00:04:00 +0800

    Remove org-hydra

--- modules/happy-hydra.el
+++ modules/happy-hydra.el
@@ -9,20 +9,6 @@
 (setup (:recipe 'hydra)
   (:require hydra))
 
-(defhydra org-hydra (:color blue :hint nil)
-  "
-
-  org-hydra
-
-  _s_ : org-schedule
-  _,_ : org-insert-structure-template
-
-  _q_ : keyboard-quit
-  "
-  ("s" org-schedule)
-  ("," org-insert-structure-template)
-  ("q" keyboard-quit))
-
 (defhydra roam-hydra (:hint nil)
   "
 
@@ -40,7 +26,6 @@
   ("t" org-roam-tag-add :exit t)
   ("q" keyboard-quit :exit t))
 
-(define-key org-mode-map (kbd "C-c o") 'org-hydra/body)
 (define-key global-map (kbd "C-c n") 'roam-hydra/body)
 
 (provide 'happy-hydra)
